c08df80a2b11ca622b2ef62968605edd29a912be
mahesh-muttinti <maheshmuttinti@gmail.com>
2021-01-15 17:46:56 +0530

    added successfully counter app

---
+++ simple-counter/src/App.js
@@ -0,0 +1,12 @@
+
+function CounterApp() {
+  return (
+    <>
+	<h1>Hello I am from React and loaded in github page</h1>
+	<p>This is actually counter app but it was tested whether the github is able to deploy this react app or not</p>
+	<h2>Finally, It works</h2>
+    </>
+  );
+}
+
+export default CounterApp;
